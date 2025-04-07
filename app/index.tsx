b4afa56f7e784ba185156ac36cd267fe177c890e
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-04-07 16:05:06 +0200

    Load Decks and display

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,43 @@
-import {Text} from "react-native";
+import {ScrollView, StyleSheet, Text, View} from "react-native";
+import {useCallback, useState} from "react";
+import {Deck} from "@/constants/Types";
+import {useFocusEffect} from "expo-router";
 
 const Index = () => {
+    const [decks, setDecks] = useState<Deck[]>([]);
+
+    useFocusEffect(useCallback(() => {
+        fetch("https://flashcard.darki.dev/api/decks")
+            .then(res => res.json())
+            .then(setDecks)
+            .catch(e => {
+                alert("There was a problem while loading the deck!");
+                console.error(e);
+            });
+    }, []));
+
     return (
-        <Text>Hey</Text>
+        <ScrollView>
+            <Text style={styles.title}>Hallo!</Text>
+            <View style={styles.container}>
+                {decks.map(d => <Text>{d.name}</Text>)}
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginVertical: 20,
+    }
+});
 
 export default Index;
